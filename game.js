da142f568ea94bd845bab95824cb449a61de29fd
cezarytworzewski92 <cezary.tworzewski@gmail.com>
2018-01-07 13:02:14 +0100

    Port handling of cell clicks to view-Controller

--- game.js
+++ game.js
@@ -11,34 +11,52 @@ let onClick = function onClick() {
 cell.addEventListener('click', onClick);*/
 'use strict';
 
-class Component{
-    getElement() { 
+class Component {
+    getElement() {
         return this._element;
     }
 }
 
 class CellComponent extends Component {
-    constructor() {
+    constructor(handleCellClick) {
         super(); //wywołuje konstruktor bazowy
         this._state = 'unknown';
         this._element = document.createElement('td');
         const self = this;
-        this._element.addEventListener('click', function() {
-            this.setState('miss');
-        }.bind(this));
+        this._element.addEventListener('click', function () {
+            handleCellClick();
+        });
         this._refresh();
     }
-    
+
     setState(stateName) {
         this._state = stateName;
         this._refresh();
-    }  
-    
+    }
+
     _refresh() {
         this._element.textContent = this._state;
         this._element.className = 'cell_' + this._state;
     }
 }
 
-const myCell = new CellComponent();
+class GameController {
+    constructor(cell) {
+        this._cell = cell;
+    }
+
+    handleCellClick() {
+        this._cell.setState('miss');
+    }
+}
+
+let myController;
+
+function handleCellClick() {
+    myController.handleCellClick();
+}
+
+
+const myCell = new CellComponent(handleCellClick);
+myController = new GameController(myCell);
 document.getElementById('cellContainer').appendChild(myCell.getElement());
